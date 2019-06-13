cf4892b85091b81a7876df8a42d2d42714904148
Kumbaya1 <790023580@qq.com>
2019-06-13 19:28:39 +0800

    fix(main.js):修复elementui message 组件按需加载默认自动弹出问题

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import "./assets/css/reset.css"
 import '@/assets/iconfont/iconfont.css'
 
 
-import { Button, Menu, Submenu, MenuItem, Form, FormItem, Input } from "element-ui"
-Vue.use(Button).use(Menu).use(Submenu).use(MenuItem).use(Form).use(FormItem).use(Input)
+import { Button, Menu, Submenu, MenuItem, Form, FormItem, Input, Message } from "element-ui"
+Vue.use(Button).use(Menu).use(Submenu).use(MenuItem).use(Form).use(FormItem).use(Input);
+// 解决刷新自动弹出问题
+Vue.component(Message);
+Vue.prototype.$message = Message;
 Vue.config.productionTip = false
 
 new Vue({
